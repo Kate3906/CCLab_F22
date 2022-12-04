b45301458b6693bf4f06cbb61be919dc2dc11cb0
Kate3906 <ps3906@nyu.edu>
2022-12-04 12:59:13 +0800

    upload files

---
+++ image-sound/sketch.js
@@ -0,0 +1,29 @@
+let img;
+function preload() {
+  img = loadImage("cclab-assets/images/colorful.jpg")
+}
+
+function setup() {
+  let canvas = createCanvas(500, 281);
+  canvas.parent("canvasContainer");
+  background(220);
+
+
+
+}
+
+
+function draw() {
+  //ellipse(100, 100, 100, 100);
+  imagemode
+  imagemode
+  imagemode
+  tint
+  Image(img, mouseX, mouseY, 100, 100);
+  filter threshold
+}
+
+/*function buttonClicked() {
+  console.log("Button Clicked!");
+  background(random(255));
+}*/
